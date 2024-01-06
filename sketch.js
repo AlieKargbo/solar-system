d9320295b599c6a130a344e55f6c110752469fde
Aliekay <aliekargbo000@gmail.com>
2024-01-06 02:58:06 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -145,7 +145,7 @@ var startupSound;
          
 function preload() {
     soundFormats('mp3');
-    startupSound=loadSound('./i.mp3');
+    startupSound=loadSound('./Interstellar2.mp3');
 }
          
 function music() {
